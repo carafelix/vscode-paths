396db91cd649b49f55571b9d368d855ca53addf1
Hero Protagonist <heroprotagonist32@gmail.com>
2024-04-14 13:41:15 -0400

    update pointers

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,21 @@
 import * as vscode from 'vscode';
+import { Utils as UriResolve } from 'vscode-uri'; 
 export function activate(context: vscode.ExtensionContext) {
 	
 	const extFolder = vscode.commands.registerCommand('vscode-paths.openExtensionsFolder', () => {
-		vscode.commands.executeCommand('revealFileInOS', vscode.Uri.file(context.extensionPath));
+			vscode.commands.executeCommand('revealFileInOS',
+				context.extension.extensionUri)
+			.then((_)=>{},(err)=>{
+				vscode.window.showErrorMessage(`${err}`)
+			})
 	});
 
 	const installFolder = vscode.commands.registerCommand('vscode-paths.openInstallationFolder', () => {
-		vscode.commands.executeCommand('revealFileInOS', vscode.Uri.file(vscode.env.appRoot));
+			vscode.commands.executeCommand('revealFileInOS',
+				UriResolve.dirname(vscode.Uri.file(vscode.env.appRoot)))
+			.then((_)=>{},(err)=>{
+				vscode.window.showErrorMessage(`${err}`)
+			})
 	});
 
 	context.subscriptions.push(extFolder,installFolder);
